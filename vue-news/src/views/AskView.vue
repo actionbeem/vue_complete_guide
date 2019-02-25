a104c8520b65b40819b0eb3cebf45d83b350ac79
김종혁 <actionbeem929@gmail.com>
2019-02-25 11:48:14 +0900

    api -> store로 구현 중

---
+++ vue-news/src/views/AskView.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <div v-for="item in items">{{ item.title }}</div>  
+  </div>
+</template>
+
+<script>
+import { fetchAskList } from '../api/index.js'
+
+export default {
+  data(){
+    return {
+      items: []
+    }
+  },
+  created(){
+    var vm = this;
+    fetchAskList()
+      .then(function(response){
+         console.log(response)
+        vm.items = response.data
+      })
+      .catch(function(err){
+        console.log(err)
+      })
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ vue-news/src/views/JobsView.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <div v-for="item in items">{{ item.title }}</div>
+  </div>
+</template>
+
+<script>
+import { fetchJobsList } from '../api/index.js'
+
+export default {
+  data(){
+    return {
+      items: []
+    }
+  },
+  created(){
+    fetchJobsList()
+      .then(response => this.items = response.data)
+      .catch(error => console.log(error))
+  }
+}
+</script>
+
+<style>
+
+</style>
