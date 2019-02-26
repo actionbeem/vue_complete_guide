90c5d15eb3899b45ae90e5b6f7e1e5957797eab4
김종혁 <actionbeem929@gmail.com>
2019-02-26 17:21:15 +0900

    리팩토링1 - 리스트 아이템 ... 섹션 완료

--- vue-news/src/views/AskView.vue
+++ vue-news/src/views/AskView.vue
@@ -1,12 +1,11 @@
 <template>
   <div> 
-    <ul class="news-list">
+    <list-item></list-item>
+    <!-- <ul class="news-list">
       <li v-for="item in fetchedAsk" class="post">
-        <!-- 포인트 영역 -->
         <div class="points">
           {{ item.points }}
         </div>
-        <!-- 기타 정보 영역 -->
         <div>
           <p class="news-title">
             <router-link v-bind:to="`/item/${item.id}`">
@@ -19,27 +18,24 @@
           </small>
         </div>
       </li>
-    </ul>
+    </ul> -->
   </div>
 </template>
 
 <script>
-import { mapState, mapGetters } from 'vuex'
+import ListItem from '../components/ListItem.vue'
+// import { mapState, mapGetters } from 'vuex'
 
 export default {
-  computed: {
-    ...mapGetters(['fetchedAsk'])
-  },
-  created(){
-    this.$store.dispatch('FETCH_ASK')
+  components: {
+    ListItem,
   }
+  // computed: {
+  //   ...mapGetters(['fetchedAsk'])
+  // },
+  // created(){
+  //   this.$store.dispatch('FETCH_ASK')
+  // }
 }
 </script>
 
-<style scoped>
-.news-list { margin:0; padding:0; }
-.post { list-style:none; display:flex; align-items:center; border-bottom:1px solid #eee; }
-.points { width:80px; height:60px; display:flex; align-items:center; justify-content:center; color:cadetblue; }
-.news-title { margin:0; }
-.link-text { color:#828282; }
-</style>
