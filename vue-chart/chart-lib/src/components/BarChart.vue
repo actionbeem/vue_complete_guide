4e3db7c38a8783dd0718fd30b40dc213eeaf0a8b
김종혁 <actionbeem929@gmail.com>
2019-02-28 16:25:46 +0900

    외부 라이브러리 모듈화 .. 섹션 완료

---
+++ vue-chart/chart-lib/src/components/BarChart.vue
@@ -0,0 +1,32 @@
+<template>
+  <canvas ref="barChart" id="barChart" width="400" height="400"></canvas>
+</template>
+
+<script>
+export default {
+    props: ['propsdata'],
+  mounted(){
+    var ctx = this.$refs.barChart;
+    var myChart = new this.$_Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+            datasets: this.propsdata
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero:true
+                    }
+                }]
+            }
+        }
+    });
+  }
+}
+</script>
+
+<style>
+
+</style>
