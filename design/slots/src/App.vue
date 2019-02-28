4cbc0a920be8583b3dfe4738f8771cfcdf724ee6
김종혁 <actionbeem929@gmail.com>
2019-02-28 18:44:15 +0900

    컴포넌트 디자인 패턴 .. 섹션 완료

--- design/slots/src/App.vue
+++ design/slots/src/App.vue
@@ -1,11 +1,23 @@
 <template>
   <div>
     <ul>
-      <item>아이템 1</item>
-      <item>아이템 2</item>
-      <item>아이템 3</item>
-      <item>아이템 4</item>
-      <item>아이템 5</item>
+      <item>
+        아이템 1
+      </item>
+      <item>
+        아이템 2 <button>click  me</button>
+      </item>
+      <item>
+        <div>
+          아이템 3
+        </div>
+        <img src="./assets/endgame.png" alt="">
+      </item>
+      <item>
+        <div style="color:blue">
+          아이템 4
+        </div>
+      </item>
     </ul>
   </div>
 </template>
@@ -16,6 +28,11 @@ import Item from './Item.vue';
 export default {
   components: {
     Item,
+  },
+  data(){
+    return {
+      items: ["item01","item02","item03","item04","item05"]
+    }
   },
 }
 </script>
--- design/slots/src/Item.vue
+++ design/slots/src/Item.vue
@@ -1,7 +1,7 @@
 <template>
   <li>
     <slot>
-      <!-- NOTE: 등록하는 곳에서 정의할 화면 영역 -->
+      <!-- 등록하는 곳에서 정의할 화면 영역 -->
     </slot>
   </li>
 </template>
