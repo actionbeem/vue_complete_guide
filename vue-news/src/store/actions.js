d83b7df27030816e35e3fde7b035aaee025684aa
김종혁 <actionbeem929@gmail.com>
2019-02-28 14:42:07 +0900

    리팩토링5 - async & await ... 섹션 완료

--- vue-news/src/store/actions.js
+++ vue-news/src/store/actions.js
@@ -8,34 +8,26 @@ import {
 } from '../api/index.js'
 
 export default {
-  FETCH_NEWS(context){
-    return fetchNewsList()
-      .then(response => {
-        context.commit('SET_NEWS', response.data);
-        return response;
-      })
-      .catch(error => {
-        console.log(error)
-      })
+  async FETCH_NEWS(context){
+    const response = await fetchNewsList();
+    context.commit('SET_NEWS', response.data)
+    return response;
   },
-  FETCH_ASK({ commit }){
-    return fetchAskList()
-      .then(({ data }) => {
-        commit('SET_ASK', data)
-      })
-      .catch(error => {
-        console.log(error)
-      })
+  async FECTH_JOBS({commit}){
+    try {
+      const response = await fetchJobsList();
+      commit('SET_JOBS',response.data)
+      return response;
+    } catch (error) {
+      console.log(error)
+    }
   },
-  FETCH_JOBS({ commit }){
-    return fetchJobsList()
-      .then(({ data }) => {
-        commit('SET_JOBS', data)
-      })
-      .catch(error => {
-        console.log(error)
-      })
+  async FETCH_ASK({ commit }){
+    const response = await fetchAskList();
+    commit('SET_ASK',response.data )
+    return response;
   },
+
   FETCH_USER({ commit }, name){
     return fetchUserInfo(name)
       .then(({ data }) => {
@@ -55,9 +47,15 @@ export default {
         console.log(error)
       })
   },
-  FETCH_LIST({ commit }, pageName){
-    return fetchList(pageName)
-      .then(({ data }) => commit('SET_LIST', data))
-      .catch(error => console.log('여긴가:' , error))
-  },
+  // FETCH_LIST({ commit }, pageName){
+  //   return fetchList(pageName)
+  //     .then(({ data }) => commit('SET_LIST', data))
+  //     .catch(error => console.log('여긴가:' , error))
+  // },
+
+  async FETCH_LIST({ commit }, pageName){
+    const response = await fetchList(pageName);
+    commit('SET_LIST', response.data);
+    return response;
+  }
 }
